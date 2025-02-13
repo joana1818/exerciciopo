cd513eae00ac975659b5ed779748e7b513ac1503
joana1818 <161776671+joana1818@users.noreply.github.com>
2025-02-13 16:14:00 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -23,7 +23,31 @@ public class Funcionario {
     }
 
     public void calcularSalario(){
-        return getSalario() + bonus;
+        return salario;
+    }
+}
+
+
+class Gerente extends Funcionario {
+    private double bonus;
+
+    public Gerente(String nome, double salario, double bonus) {
+        super(nome, salario);
+        this.bonus = bonus;
     }
 
+    @Override
+    public double calcularSalario() {
+        return getSalario() + bonus;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Joana", 2222);
+        Gerente gerente = new Gerente("Kailane", 12344, 98383);
+
+        System.out.println(funcionario.getNome() + funcionario.getSalario());
+        System.out.println(gerente.getNome() + gerente.calcularSalario());
+    }
 }
